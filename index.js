bf746bddf10cde51bf4b1940a6eed0a790ef973f
shoaib Nadeem <140244379+shoaib1600@users.noreply.github.com>
2023-12-17 20:07:22 +0500

    NavBar

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
